688bb8bc89cf2cd342b1a03bb5883ca407b92ba6
Keerthi Reddy <keerthi24092212@gmail.com>
2026-07-20 19:25:46 +0530

    Time: 42 ms (71.93%) | Memory: 33.9 MB (82.46%) - LeetSync

--- 3954-maximum-balanced-shipments/maximum-balanced-shipments.py
+++ 3954-maximum-balanced-shipments/maximum-balanced-shipments.py
@@ -1,17 +1,27 @@
 class Solution:
     def maxBalancedShipments(self, w: List[int]) -> int:
-        stack=[]
+        # stack=[]
+        # ans=0
+        # for i in range(len(w)):
+        #     if not stack:
+        #         stack.append(w[i])
+        #     else:
+        #         if w[i]>=stack[-1]:
+        #             stack.append(w[i])
+        #         else:
+        #             ans+=1
+        #             while stack:
+        #                 stack.pop()
+        # return ans
+
         ans=0
-        for i in range(len(w)):
-            if not stack:
-                stack.append(w[i])
+        m=0
+        for i in w:
+            if i<m:
+                ans+=1
+                m=0
             else:
-                if w[i]>=stack[-1]:
-                    stack.append(w[i])
-                else:
-                    ans+=1
-                    while stack:
-                        stack.pop()
+                m=i
         return ans
                     
                 
