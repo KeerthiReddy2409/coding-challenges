5edd42d5951920f5720e1c62c98c7aec5d1e99da
Keerthi Reddy <keerthi24092212@gmail.com>
2026-08-11 19:28:42 +0530

    Time: 59 ms (7.68%) | Memory: 19.3 MB (44.19%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,17 +1,19 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        i=0
-        ans=""
-        n=len(command)
-        while i<n:
-            if command[i]=="G":
-                ans+="G"
-                i+=1
-            else:
-                if i+1<n and command[i+1]==")":
-                    ans+="o"
-                    i+=2
-                else:
-                    ans+="al"
-                    i+=4
-        return ans
+        # i=0
+        # ans=""
+        # n=len(command)
+        # while i<n:
+        #     if command[i]=="G":
+        #         ans+="G"
+        #         i+=1
+        #     else:
+        #         if i+1<n and command[i+1]==")":
+        #             ans+="o"
+        #             i+=2
+        #         else:
+        #             ans+="al"
+        #             i+=4
+        # return ans
+
+        return command.replace("()", "o").replace("(al)", "al")
