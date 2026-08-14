85e90af7d948b8b91139bd8f3922b3891864d524
Keerthi Reddy <keerthi24092212@gmail.com>
2026-08-14 06:09:58 +0530

    Time: 38 ms (55.97%) | Memory: 22.2 MB (52.64%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfitAssignment(self,difficulty:List[int],profit:List[int],worker:List[int])->int:
+        jobs=sorted(zip(difficulty,profit))
+        worker.sort()
+        ans=best=i=0
+        for w in worker:
+            while i<len(jobs) and jobs[i][0]<=w:
+                best=max(best,jobs[i][1])
+                i+=1
+            ans+=best
+        return ans
